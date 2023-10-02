29d0c41f22cd38833afa134105cde32f120d1d44
pdlanl <anil@truemark.dev>
2023-10-02 15:26:00 +0545

    - initialize project   - add: basic commands   - add: commander and figlet libraries

---
+++ dist/index.js
@@ -0,0 +1,50 @@
+#! /usr/bin/env node
+"use strict";
+const { Command } = require("commander");
+const figlet = require("figlet");
+const program = new Command();
+console.log(figlet.textSync("AleoJS"));
+program
+    .version('1.0.0')
+    .description('AleoJS CLI');
+program
+    .command('init')
+    .description('Initialize your AleoJS project')
+    .action(() => {
+    console.log('Initializing AleoJS project...');
+    // Add your initialization logic here
+});
+program
+    .command('add')
+    .description('Add a new component or resource')
+    .option('-c, --component <name>', 'Add a new component')
+    .option('-r, --resource <name>', 'Add a new resource')
+    .action((cmd) => {
+    if (cmd.component) {
+        console.log(`Adding component: ${cmd.component}`);
+        // Add logic for adding a component here
+    }
+    else if (cmd.resource) {
+        console.log(`Adding resource: ${cmd.resource}`);
+        // Add logic for adding a resource here
+    }
+    else {
+        console.log('No action specified. Use either -c or -r.');
+    }
+});
+program
+    .command('compile')
+    .description('Compile your AleoJS project')
+    .action(() => {
+    console.log('Compiling AleoJS project...');
+    // Add your compilation logic here
+});
+program
+    .command('run')
+    .description('Run your AleoJS project')
+    .action(() => {
+    console.log('Running AleoJS project...');
+    // Add your run logic here
+});
+program.parse(process.argv);
+//# sourceMappingURL=index.js.map
