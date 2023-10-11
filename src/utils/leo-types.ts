93f12aed30c7d9e72da3fadeffa02b4bab934be9
Sujeet Silwal <silwalsujeet149@gmail.com>
2023-10-11 16:07:22 +0545

    * add aleo to js conversion function for int type

--- src/utils/leo-types.ts
+++ src/utils/leo-types.ts
@@ -90,18 +90,21 @@ export const group = (value: BigInt): LeoGroup => {
 
 export const u8 = (value: number): LeoU8 => {
     if (isNaN(value)) throw new Error("u8 parsing failed");
+    if (value < 0 || value > ((1 << 8) - 1)) throw new Error('Exceed max uint8 value: ' + value);
     const parsed = value + "u8";
     return leoU8Schema.parse(parsed);
 };
 
 export const u16 = (value: number): LeoU16 => {
     if (isNaN(value)) throw new Error("u16 parsing failed");
+    if (value < 0 || value > ((1 << 16) - 1)) throw new Error('Exceed max uint16 value: ' + value);
     const parsed = value + "u16";
     return leoU16Schema.parse(parsed);
 };
 
 export const u32 = (value: number): LeoU32 => {
     if (isNaN(value)) throw new Error("u32 parsing failed");
+    if (value < 0 || value > ((1 << 32) - 1)) throw new Error('Exceed max uint32 value: ' + value);
     const parsed = value + "u32";
     return leoU32Schema.parse(parsed);
 };
@@ -117,6 +120,37 @@ export const u128 = (value: BigInt): LeoU128 => {
     const parsed = value + "u128";
     return leoU128Schema.parse(parsed);
 };
+
+export const i8 = (value: number): LeoU8 => {
+    if (isNaN(value)) throw new Error("u8 parsing failed");
+    const parsed = value + "i8";
+    return leoU8Schema.parse(parsed);
+};
+
+export const i16 = (value: number): LeoU16 => {
+    if (isNaN(value)) throw new Error("u16 parsing failed");
+    const parsed = value + "i16";
+    return leoU16Schema.parse(parsed);
+};
+
+export const i32 = (value: number): LeoU32 => {
+    if (isNaN(value)) throw new Error("u32 parsing failed");
+    const parsed = value + "i32";
+    return leoU32Schema.parse(parsed);
+};
+
+export const i64 = (value: number): LeoU64 => {
+    if (isNaN(value)) throw new Error("u64 parsing failed");
+    const parsed = value + "i64";
+    return leoU64Schema.parse(parsed);
+};
+
+export const i128 = (value: BigInt): LeoU128 => {
+    if (!value) throw new Error("u128 parsing failed");
+    const parsed = value + "i128";
+    return leoU128Schema.parse(parsed);
+};
+
 
 export const boolean = (value: boolean): LeoU128 => {
     const val = value ? "true" : "false";
