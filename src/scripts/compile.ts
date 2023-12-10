44aee35808b74fdd7a307e98da3e57f72c675299
pdlanl <anil@truemark.dev>
2023-12-10 18:34:09 +0530

    * Handle imports on contract
      - update: build logic to add imports on compiliation

--- src/scripts/compile.ts
+++ src/scripts/compile.ts
@@ -5,16 +5,50 @@ import { getProjectRoot } from '../utils/fs-utils';
 import { toSnakeCase } from '../utils/formatters';
 import Shell from '../utils/shell';
 
-const GENERATE_FILE_OUT_DIR = 'artifacts/';
+const GENERATE_FILE_OUT_DIR = 'artifacts';
 const LEO_ARTIFACTS = `${GENERATE_FILE_OUT_DIR}/leo`;
 const JS_ARTIFACTS = `${GENERATE_FILE_OUT_DIR}/js`;
 const PROGRAM_DIRECTORY = './programs/';
 
+async function getFileImports(filePath: string) {
+  const code = fs.readFileSync(filePath, 'utf-8');
+
+  const regex = /import\s+([\w.]+);/g;
+  const matches = [];
+  let match;
+
+  while ((match = regex.exec(code)) !== null) {
+    matches.push(match[1]);
+  }
+
+  return matches;
+}
+
 async function buildProgram(programName: string) {
   const parsedProgramName = toSnakeCase(programName);
   const projectRoot = getProjectRoot();
-  const command = `mkdir -p "${projectRoot}/${LEO_ARTIFACTS}" && cd "${projectRoot}/${LEO_ARTIFACTS}" && leo new ${parsedProgramName} && rm "${projectRoot}/${LEO_ARTIFACTS}/${parsedProgramName}/src/main.leo" && cp "${projectRoot}/programs/${parsedProgramName}.leo" "${projectRoot}/${LEO_ARTIFACTS}/${parsedProgramName}/src/main.leo" && cd "${projectRoot}/${LEO_ARTIFACTS}/${parsedProgramName}" && leo run`;
-  const shellCommand = new Shell(command);
+  const fileImports = await getFileImports(
+    `${projectRoot}/programs/${programName}.leo`
+  );
+  const createLeoCommand = `mkdir -p "${projectRoot}/${LEO_ARTIFACTS}" && cd "${projectRoot}/${LEO_ARTIFACTS}" && leo new ${parsedProgramName} && rm "${projectRoot}/${LEO_ARTIFACTS}/${parsedProgramName}/src/main.leo" && cp "${projectRoot}/programs/${parsedProgramName}.leo" "${projectRoot}/${LEO_ARTIFACTS}/${parsedProgramName}/src/main.leo"`;
+  const leoShellCommand = new Shell(createLeoCommand);
+  const res = await leoShellCommand.asyncExec();
+
+  if (fileImports.length) {
+    const copyImportsPath = fileImports.map(
+      (fileImport) => `"${projectRoot}/programs/${fileImport}"`
+    );
+
+    const createimports = `mkdir "${projectRoot}/${LEO_ARTIFACTS}/${programName}/imports" && cp ${copyImportsPath.join(
+      ' '
+    )} "${projectRoot}/${LEO_ARTIFACTS}/${parsedProgramName}/imports"`;
+    console.log(createimports);
+    const importShellCommand = new Shell(createimports);
+    await importShellCommand.asyncExec();
+  }
+
+  const leoRunCommand = `cd "${projectRoot}/${LEO_ARTIFACTS}/${parsedProgramName}" && leo run`;
+  const shellCommand = new Shell(leoRunCommand);
   return shellCommand.asyncExec();
 }
 
