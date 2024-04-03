c4e5f12dea7a13932b74a145807e6950b7dd8f3f
Serafim Gerasimov <serafimgerasimov44@gmail.com>
2024-04-03 17:12:28 +0500

    fix: external call detection

--- packages/core/src/parser/parser.ts
+++ packages/core/src/parser/parser.ts
@@ -146,7 +146,7 @@ class Parser {
       if (tk.value === CALL_OPERATOR) {
         const calledFunction = this.tokenizer.readToken().value;
         const parts = calledFunction.split('.aleo/');
-        if (!parts[0].startsWith(currentProgramName)) {
+        if (parts[0] !== currentProgramName) {
           functionDef.calls.push({
             program: parts[0],
             functionName: parts[1]
