a2e9c8cb7aa005420dc39cc06b098678236af406
Serafim Gerasimov <serafimgerasimov44@gmail.com>
2024-06-25 13:09:47 +0500

    fix: prevent imported code from leo execution

--- packages/core/src/execution/execution.ts
+++ packages/core/src/execution/execution.ts
@@ -1,19 +1,25 @@
-import { isDefined } from "./execution-helper";
-import { LeoExecuteContext } from "./leo-execute";
-import { LeoRunContext } from "./leo-run";
-import { SnarkExecuteContext } from "./snark-execute";
-import { ContractConfig, ExecutionContext, ExecutionMode } from "./types";
+import { isDefined } from './execution-helper';
+import { LeoExecuteContext } from './leo-execute';
+import { LeoRunContext } from './leo-run';
+import { SnarkExecuteContext } from './snark-execute';
+import { ContractConfig, ExecutionContext, ExecutionMode } from './types';
 
-export function CreateExecutionContext(config: ContractConfig): ExecutionContext {
-    if (!isDefined(config.mode)) throw Error("Execution mode not selected in contract config");
-    switch (config.mode) {
-        case ExecutionMode.LeoRun:
-            return new LeoRunContext(config);
-        case ExecutionMode.LeoExecute:
-            return new LeoExecuteContext(config);
-        case ExecutionMode.SnarkExecute:
-            return new SnarkExecuteContext(config);
-
-    }
-    throw new Error('Unsupported Execution Mode');
+export function CreateExecutionContext(
+  config: ContractConfig
+): ExecutionContext {
+  if (!isDefined(config.mode))
+    throw Error('Execution mode not selected in contract config');
+  if (config.isImportedAleo && config.mode !== ExecutionMode.SnarkExecute)
+    throw Error(
+      "Execution of imported code is possible only with 'SnarkExecute' mode"
+    );
+  switch (config.mode) {
+    case ExecutionMode.LeoRun:
+      return new LeoRunContext(config);
+    case ExecutionMode.LeoExecute:
+      return new LeoExecuteContext(config);
+    case ExecutionMode.SnarkExecute:
+      return new SnarkExecuteContext(config);
+  }
+  throw new Error('Unsupported Execution Mode');
 }
