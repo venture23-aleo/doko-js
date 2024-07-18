ef9f8bb3d379acde6ded2310a3dc3140242f37ad
Serafim Gerasimov <serafimgerasimov44@gmail.com>
2024-07-18 14:12:37 +0300

    fix: leo version check

--- packages/cli/src/scripts/compile.ts
+++ packages/cli/src/scripts/compile.ts
@@ -10,6 +10,8 @@ import {
   Shell
 } from '@doko-js/utils';
 import { Node, NodeImport, sort } from '@/utils/graph';
+import { promisify } from 'util';
+import { exec } from 'child_process';
 
 const GENERATE_FILE_OUT_DIR = 'artifacts';
 const LEO_ARTIFACTS = `${GENERATE_FILE_OUT_DIR}/leo`;
@@ -195,7 +197,15 @@ async function cachePrograms(
   return leoShellCommand.asyncExec();
 }
 
-async function buildProgram(programName: string) {
+async function getLeoVersion(): Promise<string> {
+  const execute = promisify(exec);
+  const cmd = 'leo -V';
+  const { stdout } = await execute(cmd);
+  const searchResult = /leo (?<version>\d+\.\d+\.\d+)/.exec(stdout);
+  return searchResult?.groups?.version || '';
+}
+
+async function buildProgram(programName: string, leoVersion: string) {
   const parsedProgramName = toSnakeCase(programName);
   const projectRoot = getProjectRoot();
   const artifactDir = `${projectRoot}/${LEO_ARTIFACTS}`;
@@ -238,7 +248,10 @@ async function buildProgram(programName: string) {
     }
   }
 
-  const networkFlag = defaultNetwork ? `--network ${defaultNetwork}` : '';
+  const networkFlag =
+    defaultNetwork && leoVersion.startsWith('2.')
+      ? `--network ${defaultNetwork}`
+      : '';
 
   const leoBuildCommand = `cd "${programDir}" && leo build --home ${leoHomeDir} ${networkFlag}`;
   const shellCommand = new Shell(leoBuildCommand);
@@ -265,6 +278,7 @@ async function buildPrograms() {
     const importsPath = path.join(directoryPath, IMPORTS_DIRECTORY);
     let names = getFilenamesInDirectory(programsPath).sort();
     await prepareImportsRegistry(importsPath, ALEO_DEPS_REGISTRY);
+    const leoVersion = await getLeoVersion();
 
     const leoArtifactsPath = path.join(directoryPath, LEO_ARTIFACTS);
     console.log('Cleaning up old files');
@@ -283,7 +297,7 @@ async function buildPrograms() {
     try {
       for (const name of names) {
         const programName = name.split('.')[0];
-        await buildProgram(programName);
+        await buildProgram(programName, leoVersion);
       }
       //try {
       //  const buildResults = await Promise.all(buildPromises);
